614082281d9f6e67fb628fc3b0dbc9ef6629c87c
VectorM <ua.itzakon@gmail.com>
2017-08-08 18:18:23 +0300

    socket.io

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-console.log('Step: 1')
-setTimeout(function () {
-  console.log('Step: 3')
-  console.log('Step 5')
-}, 1000);
-console.log('Step: 2')
-console.log('Step 4')
+import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
+
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.Server(app);
+const io = new SocketIO(server);
+
+let users = [];
+let connections = [];
+
+app.get('/', (req,res) => {
+  res.sendFile(`${__dirname}/src/index.html`)
+});
+
+server.listen(port, () => {
+    console.log('[INFO] Listening on *:' + port);
+});
+
+io.on('connection', (socket) => {
+  connections.push(socket)
+  socket.broadcast.emit('user connected');
+  console.log(`Connected: ${connections.length} sockets connected`)
+
+  socket.on('disconnect', (data) => {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUserNames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnected: ${connections.length} sockets connected`);
+  });
+
+  socket.on('send message', (data) => {
+    io.emit('new message', {msg: data});
+  });
+
+  socket.on('new suer', (data, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username)
+    updateUserNames();
+  });
+
+  function updateUserNames(){
+    io.sockets.emit('get users', users)
+  }
+});
